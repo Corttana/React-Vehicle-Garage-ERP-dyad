8c3fbc1fe162f4d0d92c44498cb31164660ec136
[dyad] <jabeermk@gmail.com>
2025-08-09 18:24:55 +0530

    [dyad] Removed native mobile folders and config - wrote 1 file(s), deleted 3 file(s)

--- android/app/src/main/java/com/example/reactvehiclegarageerp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.reactvehiclegarageerp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.reactvehiclegarageerp',
-  appName: 'React-Vehicle Garage ERP',
-  webDir: 'dist'
-};
-
-export default config;
